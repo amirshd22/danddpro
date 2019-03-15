b1bc3bc6b05e9d9f7dd985bdbee4f7c361205a8a
amir <amirshd2295@icloud.com>
2019-03-15 23:36:06 +0330

    almost done with the d and d website waite for it

--- danddpy/settings.py
+++ danddpy/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'kharid_konid.apps.KharidKonidConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'danddpy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "danddpy",
+        'USER': "postgres",
+        'PASSWORD': "ps409397387471",
+        'HOST':"localhost",
+        'PORT':"5432"
     }
 }
 
@@ -122,3 +127,5 @@ STATIC_DIR = 'static'
 STATICFILES_DIRS = [
     STATIC_DIR, 
 ]
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDAI_URL= '/media/'
